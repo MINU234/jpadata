a6b6c4191a3ceb692be0a9b462b590f5a012b4ba
MINU234 <kmw365849@gmail.com>
2025-01-09 18:41:55 +0900

    스프링데이터) 공통인터페이스 분석

--- src/main/java/study/data_jpa/repository/TeamRepository.java
+++ src/main/java/study/data_jpa/repository/TeamRepository.java
@@ -1,41 +1,8 @@
 package study.data_jpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.data_jpa.entity.Team;
 
-import java.util.List;
-import java.util.Optional;
+public interface TeamRepository extends JpaRepository<Team, Long> {
 
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t", Team.class)
-                .getResultList();
-    }
-
-    public Optional<Team> findById(int id) { // 아이디를 이용해 찾게 될때 아이디가 NULL 값일때를 대비하여 Optional을 사용 (널 세이프티랑 비슷한 느낌?)
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long Count(){
-        return em.createQuery(
-                "select count(t) from Team t",Long.class
-        ).getSingleResult();
-    }
 }
