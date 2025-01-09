8ae114310df29c0edc99f82b2d7b8aaaa4b28250
MINU234 <kmw365849@gmail.com>
2025-01-09 17:52:39 +0900

    스프링데이터) 예제 도메인 모델과 동작확인

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -19,5 +19,19 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username) {
+        this.username = username;
+    }
+    public Member(String username, int age,Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
 
 }
