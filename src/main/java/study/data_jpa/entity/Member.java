19fb52046cbb4d9c5995883b2cef87b0e9a109f9
MINU234 <kmw365849@gmail.com>
2025-01-09 17:35:11 +0900

    hi

---
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,23 @@
+package study.data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"}) //team을 안적는 이유 team과 Member는 연관관계이기 때문에 서로 참조를 하다보면 무한루프가 될 수있다.
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+}
