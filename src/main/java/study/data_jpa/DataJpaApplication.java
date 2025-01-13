16582d86682fff9cf7963cf0eeed16c1fda6bd56
MINU234 <kmw365849@gmail.com>
2025-01-14 02:06:42 +0900

    스프링데이터) Web확장 도메인 클래스 컨버터

--- src/main/java/study/data_jpa/DataJpaApplication.java
+++ src/main/java/study/data_jpa/DataJpaApplication.java
@@ -3,6 +3,11 @@ package study.data_jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DataJpaApplication {
@@ -11,4 +16,8 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
